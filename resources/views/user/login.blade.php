c92cbe876641aceea6ad0cd838db6a2677902512
Spiros <spiroszoupas@hotmail.com>
2023-10-25 11:57:17 +0100

    create login view for testing

---
+++ resources/views/user/login.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('title', 'Login Page')
+
+@section('content')
+<body>
+    <h1>Login</h1>
+
+    @if ($errors->any())
+        <div>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+    
+    <form method="POST" action="{{ route('login.post') }}">
+        @csrf
+        <div>
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" value="{{ old('email') }}" required autofocus>
+        </div>
+
+        <div>
+            <label for="password">Password</label>
+            <input type="password" id="password" name="password" required>
+        </div>
+
+        <div>
+            <button type="submit">Login</button>
+        </div>
+    </form>
+</body>
+@endsection
