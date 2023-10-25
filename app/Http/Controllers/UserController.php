503b219d2aff3ab863e4c2ebe7728a22975b8f0c
Spiros <spiroszoupas@hotmail.com>
2023-10-25 11:44:02 +0100

    implement login functions, user views now found under user/view

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -5,11 +5,12 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function register() {
-        return view('register');
+        return view('user.register');
     }
 
     public function registerPost(Request $request) {
@@ -34,5 +35,30 @@ class UserController extends Controller
         }
 
         return redirect(route('login'))->with('success','Successfully registered!');
+    }
+
+    public function login() {
+        return view('user.login');
+    }
+
+    public function loginPost(Request $request) {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $credentials = $request->only('email','password');
+
+        if(Auth::attempt($credentials)){
+            session(['user_id' => Auth::id()]);
+
+            if (!session()->has('cart')) {
+                session(['cart' => []]);
+            }
+        
+            return redirect()->intended(route('home'));
+        }
+
+        return redirect(route('login'))->with("error", "Login details incorrect!");
     }
 }
